da2e4d025e82447854df99b0d0587b910b45995c
Oriol Gual <oriol.gual@gmail.com>
2014-12-12 18:05:29 +0100

    First session (until building castle)

---
+++ Assets/__Scripts/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	static public FollowCam Instance; // a FollowCam Singleton
+	public float easing = 0.05f;
+	public Vector2 minXY = new Vector2(0,0);
+
+	// fields set dynamically
+	public GameObject poi; // The point of interest
+	float camZ; // The desired Z pos of the camera
+	
+	void Awake() {
+		Instance = this;
+		camZ = transform.position.z;
+	}
+	
+	void FixedUpdate () {
+		if (poi == null) return; // return if there is no poi
+		
+		// Get the position of the poi
+		Vector3 destination = poi.transform.position;
+
+		// Limit the X & Y to minimum values
+		destination.x = Mathf.Max( minXY.x, destination.x );
+		destination.y = Mathf.Max( minXY.y, destination.y );
+
+		destination = Vector3.Lerp (transform.position, destination, easing);
+
+		// Retain a destination.z of camZ
+		destination.z = camZ;
+
+		// Set the camera to the destination
+		transform.position = destination;
+
+		this.camera.orthographicSize = destination.y + 10;
+	}
+}
---
+++ Assets/__Scripts/Slingshot.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+
+	GameObject launchPoint;
+	Vector3 launchPos;
+	GameObject projectile;
+	bool aimingMode;
+	
+	void Awake() {
+		Transform launchPointTrans = transform.Find("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive( false );
+		launchPos = launchPointTrans.position;
+	}
+	
+	void OnMouseEnter() {
+		//print("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive( true );
+	}
+	
+	void OnMouseExit() {
+		//print("Slingshot:OnMouseExit()");
+		launchPoint.SetActive( false );
+	}	
+
+	void OnMouseDown() {
+		// The player has pressed the mouse button while over Slingshot
+		aimingMode = true;
+		// Instantiate a Projectile
+		projectile = Instantiate( prefabProjectile ) as GameObject;
+		// Start it at the launchPoint
+		projectile.transform.position = launchPos;
+		// Set it to isKinematic for now
+		projectile.rigidbody.isKinematic = true;
+	}
+
+	void Update() {
+		if (!aimingMode) return;
+
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+		if(mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize();
+			mouseDelta *= maxMagnitude;
+		}
+
+		Vector3 projPos = launchPos + mouseDelta;
+
+		if (Input.GetMouseButtonUp(0)) {
+			aimingMode = false;
+			projectile.rigidbody.isKinematic = false;
+			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
+			FollowCam.Instance.poi = projectile;
+			projectile = null;
+		}
+	}
+}
